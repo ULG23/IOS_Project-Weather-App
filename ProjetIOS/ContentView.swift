e2f6ed392f3422c0db0e9f23acc7646e49c64cea
Gauthier MIGUET <gauthiermiguet@Gauthiers-MacBook.local>
2024-02-05 13:54:10 +0100

    searching cities infos from api

--- ProjetIOS/ContentView.swift
+++ ProjetIOS/ContentView.swift
@@ -38,59 +38,78 @@ struct ContentView: View {
     }
 }
 
-/*struct BirdList: View {
-@State var filterText: String = ""
-var body: some View {
-    NavigationView {
-        List(BirdBase.shared.allBirds.filter({ filterText == "" ||
-            $0.commonName.contains(filterText)})) { bird in
-                NavigationLink(destination: Text(bird.commonName)) {
-                    BirdRow(bird: bird)
-                }
-            }
-        .listStyle(.plain)
-        .navigationTitle(Text("Oiseaux"))
-        }
-        .searchable(text: $filterText)
-        .onAppear {
-         Task {
-             await fetchBirds()
-         }
-        }
-    }
-}*/
 
 struct Bird: Identifiable, Decodable {
     var id = UUID()
     var commonName: String
     var imagePath: String
 }
+struct City: Identifiable,Decodable {
+    let id: Int
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let elevation: Double
+    let countryCode: String?
+    let featureCode: String?
+    let admin1Id: Int?
+    let admin3Id: Int?
+    let admin4Id: Int?
+    let timezone: String
+    let population: Int
+    let postcodes: [String]
+    let countryId: Int?
+    let country: String
+    let admin1: String?
+    let admin3: String?
+    let admin4: String?
 
+    private enum CodingKeys: String, CodingKey {
+        case id, name, latitude, longitude, elevation, countryCode,featureCode, admin1Id, admin3Id, admin4Id, timezone, population, postcodes, countryId, country, admin1, admin3, admin4
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        latitude = try container.decode(Double.self, forKey: .latitude)
+        longitude = try container.decode(Double.self, forKey: .longitude)
+        featureCode = try container.decodeIfPresent(String.self, forKey: .featureCode)
+        elevation = try container.decode(Double.self, forKey: .elevation)
+        countryCode = try container.decodeIfPresent(String.self, forKey: .countryCode)
+        admin1Id = try container.decodeIfPresent(Int.self, forKey: .admin1Id)
+        admin3Id = try container.decodeIfPresent(Int.self, forKey: .admin3Id)
+        admin4Id = try container.decodeIfPresent(Int.self, forKey: .admin4Id)
+        timezone = try container.decode(String.self, forKey: .timezone)
+        population = try container.decode(Int.self, forKey: .population)
+        postcodes = try container.decode([String].self, forKey: .postcodes)
+        countryId = try container.decodeIfPresent(Int.self, forKey: .countryId)
+        country = try container.decode(String.self, forKey: .country)
+        admin1 = try container.decodeIfPresent(String.self, forKey: .admin1)
+        admin3 = try container.decodeIfPresent(String.self, forKey: .admin3)
+        admin4 = try container.decodeIfPresent(String.self, forKey: .admin4)
+    }
+}
 
 struct SecondView: View {
     @State private var searchText: String = ""
-    @State private var birds: [Bird] = []
+    @State private var cities: [City] = []
 
     var body: some View {
         NavigationView {
             VStack {
-                SearchBar(text: $searchText, onSearch: fetchBirds)
-                
-                List(birds) { bird in
-                    HStack {
-                        NavigationLink(destination: Text(bird.commonName)) {
-                            Image(bird.imagePath)
-                                .resizable()
-                                .frame(width: 32, height: 32)
-                            Text(bird.commonName)
-                        }
+                SearchBar(text: $searchText, onSearch: fetchCities)
+
+                List(cities) { city in
+                    NavigationLink(destination: Text("\(city.name): Lat \(city.latitude), Lon \(city.longitude)")) {
+                        Text(city.name)
                     }
                 }
                 .listStyle(PlainListStyle())
-                .navigationTitle(Text("All ville"))
+                .navigationTitle(Text("All Cities"))
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: addBird) {
+                        Button(action: addCity) {
                             Text("Add")
                         }
                     }
@@ -99,78 +118,90 @@ struct SecondView: View {
         }
         .onAppear {
             // Fetch initial data when the view appears
-            fetchBirds()
+            fetchCities()
         }
     }
 
-    func addBird() {
+    func addCity() {
         // Implementation goes here
     }
 
-    func fetchBirds() {
+    func fetchCities() {
         guard !searchText.isEmpty else {
-            // Handle the case where searchText is empty
             return
         }
 
         Task {
             do {
-                birds = try await secondfetch(for: searchText)
+                cities = try await secondfetch(for: searchText)
             } catch {
                 print(error)
             }
         }
     }
 
-    func secondfetch(for placeName: String) async throws -> [Bird] {
-        guard let url = URL(string: "https://geocoding-api.open-meteo.com/v1/search?name=\(placeName)&count=10&language=fr&format=json") else {
+    func secondfetch(for placeName: String) async throws -> [City] {
+        guard let url = URL(string: "https://geocoding-api.open-meteo.com/v1/search?name=\(placeName)&count=2&language=fr&format=json") else {
             print("Invalid URL")
             throw ErrorPerso.invalidURL
         }
 
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            let decodedBirds = try JSONDecoder().decode([Bird].self, from: data)
-            return decodedBirds
+
+            // Decode the top-level JSON as a dictionary
+            let topLevelJSON = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+
+            // Extract the array of cities from the "results" key
+            if let results = topLevelJSON?["results"] as? [[String: Any]] {
+                // Convert the array of dictionaries to JSON data
+                let jsonData = try JSONSerialization.data(withJSONObject: results, options: [])
+                // Decode the array of City from the new JSON data
+                let decodedCities = try JSONDecoder().decode([City].self, from: jsonData)
+                return decodedCities
+            } else {
+                throw ErrorPerso.decodingFailed
+            }
         } catch {
             print(error)
             throw error
         }
     }
-}
 
 
-struct SearchBar: View {
-    @Binding var text: String
-    var onSearch: () -> Void
+    struct SearchBar: View {
+        @Binding var text: String
+        var onSearch: () -> Void
 
-    var body: some View {
-        HStack {
-            TextField("Search", text: $text, onCommit: {
-                onSearch()
-            })
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding(.horizontal)
+        var body: some View {
+            HStack {
+                TextField("Search", text: $text, onCommit: {
+                    onSearch()
+                })
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
 
-            Button(action: {
-                text = ""
-                onSearch()
-            }) {
-                Image(systemName: "xmark.circle.fill")
-                    .foregroundColor(.gray)
-                    .padding(.trailing, 8)
+                Button(action: {
+                    text = ""
+                    onSearch()
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.gray)
+                        .padding(.trailing, 8)
+                }
             }
         }
     }
-}
 
-enum ErrorPerso: Error {
-    case invalidURL
-    case decodingFailed
+    enum ErrorPerso: Error {
+        case invalidURL
+        case decodingFailed
+        // Add more error cases as needed
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SecondView()
     }
 }
